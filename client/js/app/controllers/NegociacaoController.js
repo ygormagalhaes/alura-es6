74dac31a440af4d0673d05de91c6b6a07ba719cb
Ygor Magalhaes <emaildoygor@gmail.com>
2017-10-23 23:37:03 -0200

    Implementações no controller para limpar o form após montar uma negociacação com ajuda de spread operator para construção do objeto Date

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -11,11 +11,26 @@ class NegociacaoController {
     adiciona(event) {
         event.preventDefault();
 
-        let data = new Date(this._inputData.value.split("-"));
-        console.log(data);
+        let data = new Date(
+            ...this._inputData.value.split("-")
+            .map((item, indice) => item - indice % 2));
+
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
         
-        // console.log(this._inputData.value);
-        // console.log(this._inputQuantidade.value);
-        // console.log(this._inputValor.value);
+        this.limpaFormulario();
+
+        console.log(negociacao);
+    }
+
+    limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1
+        this._inputValor.value = 0;
+
+        this._inputData.focus();
     }
 }
