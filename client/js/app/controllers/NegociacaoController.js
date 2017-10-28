db7d19e06f9789b47ff56275ccabd582eadbcfc3
Ygor Magalhaes <emaildoygor@gmail.com>
2017-10-28 19:42:14 -0200

    Criação da classe ListaNegociacoes para manipulação das negociações adicionadas pelo usuário. Encapsulamento da responsabilidade de criar uma nova negociação em um novo método de NegociacaoController.

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,25 +6,28 @@ class NegociacaoController {
         this._inputData = $("#data");
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
+
+        this._listaNegociacoes = new ListaNegociacoes();
     }
 
     adiciona(event) {
         event.preventDefault();
 
-        let negociacao = new Negociacao(
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
+
+        console.log(this._listaNegociacoes.negociacoes);
+    }
+
+    _criaNegociacao() {
+        return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         );
-        
-        this.limpaFormulario();
-
-        console.log(negociacao);
-
-        console.log(DateHelper.dataParaTexto(negociacao.data))
     }
 
-    limpaFormulario() {
+    _limpaFormulario() {
         this._inputData.value = '';
         this._inputQuantidade.value = 1
         this._inputValor.value = 0;
